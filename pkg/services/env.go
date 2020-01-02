650604e48adb879e4d8370d6abaf9b3833611299
Ivan V. Dimitrov <ivan.v.dimitrov@pm.me>
2020-01-02 13:57:30 +0200

    Add testing env

--- pkg/services/env.go
+++ pkg/services/env.go
@@ -1,27 +1,54 @@
 package services
 
 import (
+	"fmt"
 	"log"
 
 	"vanjiii/kook-book-server/pkg/auth"
 
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var fmtDBString = "host=localhost user=vanjiii dbname=%s sslmode=disable"
 
 type Env struct {
 	AuthHandler *auth.Handler
 }
 
 func NewEnv() (*Env, error) {
-	db, err := gorm.Open("postgres", "host=localhost user=vanjiii dbname=kookbook sslmode=disable")
+	db, err := gorm.Open("postgres", gormString("kookbook"))
 	if err != nil {
 		log.Fatalf("fail to create a db connection: %v", err)
 	}
 
+	return newEnv(db), err
+}
+
+func NewTestEnv() (*Env, func()) {
+	dbname := "test" + uuid.New().String()
+
+	db, err := gorm.Open("postgres", gormString(dbname))
+	if err != nil {
+		log.Fatalf("fail to create a db connection: %v", err)
+	}
+
+	cleanup := func() {
+		db.Close()
+	}
+
+	return newEnv(db), cleanup
+}
+
+func newEnv(db *gorm.DB) *Env {
 	ah := auth.NewHandler(db)
 
 	return &Env{
 		AuthHandler: ah,
-	}, err
+	}
+}
+
+func gormString(dbname string) string {
+	return fmt.Sprintf(fmtDBString, dbname)
 }
