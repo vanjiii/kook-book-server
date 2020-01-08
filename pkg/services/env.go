8eeb4717d79121c93463d217261b501925520879
Ivan V. Dimitrov <ivan.v.dimitrov@pm.me>
2020-01-08 11:12:19 +0200

    Update project; cleanup env.go

--- pkg/services/env.go
+++ pkg/services/env.go
@@ -6,7 +6,6 @@ import (
 
 	"vanjiii/kook-book-server/pkg/auth"
 
-	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -24,21 +23,6 @@ func NewEnv() (*Env, error) {
 	}
 
 	return newEnv(db), err
-}
-
-func NewTestEnv() (*Env, func()) {
-	dbname := "test" + uuid.New().String()
-
-	db, err := gorm.Open("postgres", gormString(dbname))
-	if err != nil {
-		log.Fatalf("fail to create a db connection: %v", err)
-	}
-
-	cleanup := func() {
-		db.Close()
-	}
-
-	return newEnv(db), cleanup
 }
 
 func newEnv(db *gorm.DB) *Env {
